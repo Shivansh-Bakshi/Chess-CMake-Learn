5b6fd40937f1e62a7b13c7f319e8e7eef6f3db48
Shivansh Bakshi <shivansh.bakshi@gmail.com>
2021-07-27 21:37:41 +0530

    Added arrow key input for selection

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -1,5 +1,7 @@
 #include "game.h"
+#include "../utils/keyutils.h"
 #include <iostream>
+#include <conio.h>
 
 using namespace ch::game;
 
@@ -17,14 +19,63 @@ Game::Game()
 
 void Game::Move()
 {
-    std::cout << "Select a piece (example, A 1): ";
+    std::cout << "Select a piece" << std::endl;
+    int inp = 0;
     uint8_t x, y;
-    std::cin >> y >> x;
-    y = y - 'A';
-    x = x - '1';
+    x = 0;
+    y = 0;
 
-    std::cout << "Selected Piece : " << static_cast<int>(b.getPieceAtIndex(x, y)) << "\n";
+takeInput:
     b.display(x, y);
+    inp = getch();
+    // Key pressed was a special key
+    if (inp == 224) {
+        switch(inp = getch()) {
+            case KEY_UP : {
+                std::cout << "Up" << std::endl;
+                if (x == 0) {
+                    x = 7;
+                } else {
+                    x--;
+                }
+                break;
+            }
+            case KEY_DOWN : {
+                std::cout << "Down" << std::endl;
+                if (x == 7) {
+                    x = 0;
+                } else {
+                    x++;
+                }
+                break;
+            }
+            case KEY_LEFT : {
+                std::cout << "Left" << std::endl;
+                if (y == 0) {
+                    y = 7;
+                } else {
+                    y--;
+                }
+                break;
+            }
+            case KEY_RIGHT : {
+                std::cout << "Right" << std::endl;
+                if (y == 7) {
+                    y = 0;
+                } else {
+                    y++;
+                }
+                break;
+            }
+        }
+    } else {
+        goto selectionDone;
+    }
+
+    goto takeInput;
+
+selectionDone:
+    std::cout << "Selected Piece : " << static_cast<int>(b.getPieceAtIndex(x, y)) << "\n";
 }
 
 void Game::Display()
